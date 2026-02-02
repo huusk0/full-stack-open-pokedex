b4d7578bc899aa313b4d8cd91dbacd133d8a16b7
Tuomas Huusko <huusko.tuomas@gmail.com>
2026-02-02 12:28:39 +0200

    New tests

--- e2e-tests/test1.spec.js
+++ e2e-tests/test1.spec.js
@@ -10,4 +10,19 @@ describe("Pokedex", () => {
       ),
     ).toBeVisible();
   });
+  test("pokemon page can be navigated to", async ({ page }) => {
+    await page.goto("");
+    // This targets the link that contains a div with the text "ivysaur"
+    const ivysaurLink = page.getByRole("link", { name: /ivysaur/i });
+
+    // 3. Click the link
+    // Playwright automatically waits for the element to be "actionable" (visible and not moving)
+    await ivysaurLink.click();
+    await expect(page.getByText("chlorophyll")).toBeVisible();
+    await expect(
+      page.getByText(
+        "Pokémon and Pokémon character names are trademarks of Nintendo.",
+      ),
+    ).toBeVisible();
+  });
 });
